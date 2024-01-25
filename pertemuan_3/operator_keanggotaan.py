801e3602b8b995d06ff06efc7ad803d53b89090d
Aqbil Hasya Rasyadi <140674086+Aqqubirru@users.noreply.github.com>
2024-01-25 08:53:06 +0700

    Add files via upload

---
+++ pertemuan_3/operator_keanggotaan.py
@@ -0,0 +1,7 @@
+buah = ["apel", "pisang", "durian", "anggur"]
+
+# operator membership (IN)
+print("durian" in buah)
+
+# operator membership (not in)
+print("apel" not in buah)
---
+++ pertemuan_3/operator_penugasan.py
@@ -0,0 +1,34 @@
+# penjumlahan
+a = 5
+a += 3
+print (a)
+
+# pengurangan
+b = 8
+b -= 3
+print(b)
+
+# perkalian
+c = 5
+c *= 2
+print(c)
+
+# pembagian
+d = 10
+d /= 5
+print(d)
+
+# modulus
+e = 7
+e %= 2
+print(e)
+
+# perpangkatan
+f = 5
+f **= 3
+print(f)
+
+# pembagian bulat
+g = 10
+g //= 3
+print(g)
