3c8d2350510f569c7535490bb1656669ddd73349
Aqbil Hasya Rasyadi <140674086+Aqqubirru@users.noreply.github.com>
2024-02-22 09:21:51 +0700

    Add files via upload

---
+++ pertemuan_6/perkenalan.py
@@ -0,0 +1,8 @@
+def pembukaan():
+    print("Saya siap main free fire")
+    
+def perkenalan(nama, kelas):
+    print("nama saya", nama, "dan saya kelas", kelas)
+
+pembukaan()
+perkenalan("alok", "RPL X-B")
